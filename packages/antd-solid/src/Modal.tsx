b40da0f5177e671613047b4b96437060f80c61ac
丁磊 <XNBigDevil@gmail.com>
2023-08-25 10:37:38 +0800

    ✨ feat: 优化 Modal 组件

--- packages/antd-solid/src/Modal.tsx
+++ packages/antd-solid/src/Modal.tsx
@@ -1,4 +1,4 @@
-import { type JSXElement, Show, createSignal, untrack } from 'solid-js'
+import { type JSXElement, Show, createSignal, untrack, Ref } from 'solid-js'
 import { Portal, render } from 'solid-js/web'
 import Button from './Button'
 import cs from 'classnames'
@@ -9,14 +9,13 @@ export interface ModalInstance {
 }
 
 export interface ModalProps {
-  ref?: (modal: ModalInstance) => void
+  ref?: Ref<ModalInstance>
   title?: JSXElement
   initialOpen?: boolean
   width?: string
+  height?: string
   // open?: boolean
   children?: JSXElement
-  contentClass?: string
-  closeIconClass?: string
   /**
    * 垂直居中展示 Modal
    */
@@ -55,7 +54,11 @@ function Modal(props: ModalProps) {
       setOpen(false)
     },
   }
-  untrack(() => props.ref?.(instance))
+  untrack(() => {
+    if (typeof props.ref === 'function') {
+      props.ref?.(instance)
+    }
+  })
 
   const [confirmLoading, setConfirmLoading] = createSignal(false)
 
@@ -75,30 +78,33 @@ function Modal(props: ModalProps) {
         >
           <div
             class={cs(
-              'ant-absolute ant-px-24px ant-py-20px ant-rounded-8px ant-overflow-hidden ant-bg-white',
+              'ant-absolute ant-px-24px ant-py-20px ant-rounded-8px ant-overflow-hidden ant-bg-white ant-flex ant-flex-col',
               // '!ant-[animation-duration:.5s]',
               !props.centered && 'ant-top-100px',
-              props.contentClass,
             )}
             onClick={e => {
               e.stopPropagation()
             }}
-            style={{ width: props.width ?? '520px' }}
+            style={{
+              width: props.width ?? '520px',
+              height: props.height,
+            }}
           >
             {/* 关闭按钮 */}
             <Show when={props.closeIcon !== false}>
               <Button
                 type="text"
                 class={cs(
-                  "before:ant-content-[''] before:ant-block before:i-ant-design:close-outlined ant-rm-size-btn !ant-w-22px !ant-h-22px !ant-flex !ant-justify-center !ant-items-center ant-text-center ant-text-18px !ant-absolute !ant-top-16px !ant-right-16px ant-z-1000 ant-text-[rgba(0,0,0,.45)] hover:!ant-text-[rgba(0,0,0,.88)]",
-                  props.closeIconClass,
+                  'ant-rm-size-btn !ant-w-22px !ant-h-22px !ant-flex !ant-justify-center !ant-items-center ant-text-center ant-text-18px !ant-absolute !ant-top-16px !ant-right-16px ant-z-1000 ant-text-[rgba(0,0,0,.45)] hover:!ant-text-[rgba(0,0,0,.88)]',
                 )}
                 onClick={close}
-              />
+              >
+                <span class="i-ant-design:close-outlined" />
+              </Button>
             </Show>
 
-            <div class="ant-text-[rgba(0,0,0,.88)] ant-text-16px ant-font-600 ant-mb-8px">{props.title}</div>
-            <div>{props.children}</div>
+            <div class="ant-text-[rgba(0,0,0,.88)] ant-text-16px ant-font-600">{props.title}</div>
+            <div class='ant-grow ant-my-8px'>{props.children}</div>
 
             <div class="ant-text-right">
               <Button onClick={close}>取消</Button>
