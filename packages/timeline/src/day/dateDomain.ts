2a5e4155afe919b039299ca0c8619104b04dc93b
丁磊 <XNBigDevil@gmail.com>
2023-08-25 11:51:49 +0800

    🦄 refactor: rm timeline package

--- packages/timeline/src/day/dateDomain.ts
+++
@@ -1,8 +0,0 @@
-import dayjs from 'dayjs'
-
-export default (_, __, dayjsFactory: typeof dayjs) => {
-  dayjsFactory.dateDomain = function (list: dayjs.ConfigType[]) {
-    const dayjsList = list.map(v => dayjs(v))
-    return [dayjs.min(dayjsList), dayjs.max(dayjsList)]
-  }
-}
