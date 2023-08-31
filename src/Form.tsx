8edfd1b93f60a8b3847dff9e8526556044c91a53
丁磊 <XNBigDevil@gmail.com>
2023-08-31 12:36:17 +0800

    🦄 refactor(Form): rm props.submit

--- src/Form.tsx
+++ src/Form.tsx
@@ -7,6 +7,8 @@ import {
   Show,
   Index,
   createMemo,
+  untrack,
+  type Ref,
 } from 'solid-js'
 import { Dynamic } from 'solid-js/web'
 import { toArray } from './utils/array'
@@ -17,17 +19,17 @@ export interface FormInstance<T extends {} = {}> {
 }
 
 export interface FormProps<T extends {} = {}> {
-  ref?: (form: FormInstance<T>) => void;
+  ref?: Ref<FormInstance<T>>;
   /**
    * 表单布局
    * 默认: horizontal
    */
   layout?: 'horizontal' | 'vertical' | 'inline'
-  /**
-   * 提交按钮
-   * @deprecated
-   */
-  submit?: (form: FormInstance<T>) => JSXElement
+  // /**
+  //  * 提交按钮
+  //  * @deprecated
+  //  */
+  // submit?: (form: FormInstance<T>) => JSXElement
   children: JSXElement
 }
 
@@ -53,15 +55,19 @@ function Form<T extends {} = {}>(_props: FormProps<T>) {
     return toArray(props.children) as unknown as FormItemProps[]
   })
 
-  const values = Object.fromEntries(
+  const values = untrack(() => Object.fromEntries(
     resolvedChildren().map(child => [child.name, child.initialValue]),
-  ) as T
+  ) as T)
   const formInstance: FormInstance<T> = {
     async validateFields() {
       return await Promise.resolve(values)
     },
   }
-  _props.ref?.(formInstance)
+  untrack(() => {
+    if (typeof _props.ref === 'function') {
+      _props.ref?.(formInstance)
+    }
+  })
 
   return (
     <div>
@@ -86,7 +92,7 @@ function Form<T extends {} = {}>(_props: FormProps<T>) {
         )}
       </Index>
 
-      {props.submit?.(formInstance as FormInstance<T>)}
+      {/* {props.submit?.(formInstance as FormInstance<T>)} */}
     </div>
   )
 }
