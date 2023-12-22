6bdb6f0985b2252591a85d5a18612195d7341fa3
丁磊 <XNBigDevil@gmail.com>
2023-12-22 18:08:25 +0800

    ✨ feat(Collapse): 展开收起增加动画

--- src/Collapse/index.tsx
+++ src/Collapse/index.tsx
@@ -1,5 +1,6 @@
-import { type JSXElement, type Component, For, Show, createEffect } from 'solid-js'
+import { type JSXElement, type Component, For, Show } from 'solid-js'
 import cs from 'classnames'
+import { Transition } from 'solid-transition-group'
 import { type Key } from '../types'
 import createControllableValue from '../hooks/createControllableValue'
 
@@ -21,14 +22,12 @@ const Collapse: Component<CollapseProps> = props => {
     valuePropName: 'activeKey',
     defaultValue: [],
   })
-  createEffect(() => {
-    console.log('activeKey', activeKey())
-  })
+
   return (
     <div class="ant-rounded-[var(--ant-border-radius-lg)] ant-[border:1px_solid_var(--ant-color-border)] ant-border-b-0">
       <For each={props.items}>
         {item => (
-          <div class="ant-[border-bottom:1px_solid_var(--ant-color-border)] first:ant-rounded-t-[var(--ant-border-radius-lg)] last:ant-rounded-b-[var(--ant-border-radius-lg)]">
+          <div class="ant-[border-bottom:1px_solid_var(--ant-color-border)] first:ant-rounded-t-[var(--ant-border-radius-lg)] last:ant-rounded-b-[var(--ant-border-radius-lg)] ant-cursor-pointer">
             <div
               class="ant-bg-[var(--ant-collapse-header-bg)] ant-text-[var(--ant-color-text-heading)] ant-p-[var(--ant-collapse-header-padding)]"
               onClick={() => {
@@ -49,11 +48,26 @@ const Collapse: Component<CollapseProps> = props => {
               />
               {item.label}
             </div>
-            <Show when={activeKey().includes(item.key)}>
-              <div class="ant-p-[var(--ant-padding-sm)] ant-[border-top:1px_solid_var(--ant-color-border)]">
-                {item.children}
-              </div>
-            </Show>
+            <Transition
+              onEnter={(el, done) => {
+                el.animate([{ height: '0px' }, { height: `${el.scrollHeight}px` }], {
+                  duration: 300,
+                }).finished.finally(done)
+              }}
+              onExit={(el, done) => {
+                el.animate([{ height: `${el.scrollHeight}px` }, { height: '0px' }], {
+                  duration: 300,
+                }).finished.finally(done)
+              }}
+            >
+              <Show when={activeKey().includes(item.key)}>
+                <div class="ant-overflow-hidden">
+                  <div class="ant-p-[var(--ant-padding-sm)] ant-[border-top:1px_solid_var(--ant-color-border)]">
+                    {item.children}
+                  </div>
+                </div>
+              </Show>
+            </Transition>
           </div>
         )}
       </For>
