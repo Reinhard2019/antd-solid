541690faecce825f1c96810df8e88783bf6372ec
丁磊 <XNBigDevil@gmail.com>
2023-12-26 13:46:39 +0800

    ✨ feat(Table): add empty state

--- src/Table.tsx
+++ src/Table.tsx
@@ -1,4 +1,5 @@
-import { type JSXElement, For } from 'solid-js'
+import Empty from './Empty'
+import { type JSXElement, For, Show } from 'solid-js'
 
 export interface TableColumn<R extends {}> {
   title: JSXElement
@@ -12,34 +13,40 @@ export interface TableProps<R extends {}> {
 
 const Table = <R extends {}>(props: TableProps<R>) => {
   return (
-    <table class="ant-w-full">
-      <thead>
-        <tr>
-          <For each={props.columns}>
-            {item => (
-              <th class="ant-p-16px ant-bg-[var(--light-bg-color)] ant-font-bold ant-[border-bottom:1px_solid_var(--secondary-border-color)] ant-text-left">
-                {item.title}
-              </th>
+    <div>
+      <table class="ant-w-full">
+        <thead>
+          <tr>
+            <For each={props.columns}>
+              {item => (
+                <th class="ant-p-16px ant-bg-[var(--light-bg-color)] ant-font-bold ant-[border-bottom:1px_solid_var(--secondary-border-color)] ant-text-left">
+                  {item.title}
+                </th>
+              )}
+            </For>
+          </tr>
+        </thead>
+        <tbody>
+          <For each={props.dataSource}>
+            {row => (
+              <tr class="hover:ant-bg-[var(--light-bg-color)]">
+                <For each={props.columns}>
+                  {item => (
+                    <td class="ant-p-16px ant-[border-bottom:1px_solid_var(--secondary-border-color)]">
+                      {item.render(row)}
+                    </td>
+                  )}
+                </For>
+              </tr>
             )}
           </For>
-        </tr>
-      </thead>
-      <tbody>
-        <For each={props.dataSource}>
-          {row => (
-            <tr class="hover:ant-bg-[var(--light-bg-color)]">
-              <For each={props.columns}>
-                {item => (
-                  <td class="ant-p-16px ant-[border-bottom:1px_solid_var(--secondary-border-color)]">
-                    {item.render(row)}
-                  </td>
-                )}
-              </For>
-            </tr>
-          )}
-        </For>
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+
+      <Show when={!props.dataSource.length}>
+        <Empty.PRESENTED_IMAGE_SIMPLE />
+      </Show>
+    </div>
   )
 }
 
