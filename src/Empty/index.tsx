cfa4d97a1de13e58b01a0ca7ef010f69fb67f1ed
丁磊 <XNBigDevil@gmail.com>
2023-12-26 13:45:56 +0800

    🦄 refactor(Empty): svg translate to solid

--- src/Empty/index.tsx
+++ src/Empty/index.tsx
@@ -1,14 +1,15 @@
 import { type Component } from 'solid-js'
 import PRESENTED_IMAGE_SIMPLE from './PRESENTED_IMAGE_SIMPLE'
-import emptySvg from './assets/empty.svg?raw'
+import EmptySvg from './assets/EmptySvg'
 
 const Empty: Component & {
   PRESENTED_IMAGE_SIMPLE: Component
 } = () => {
   return (
     <div>
-      {/* eslint-disable-next-line solid/no-innerhtml */}
-      <div class='ant-mb-[var(--ant-margin-xs)] ant-flex ant-justify-center' innerHTML={emptySvg} />
+      <div class='ant-mb-[var(--ant-margin-xs)] ant-flex ant-justify-center'>
+        <EmptySvg />
+      </div>
       <div class='ant-text-[var(--ant-color-text)] ant-text-center'>暂无数据</div>
     </div>
   )
