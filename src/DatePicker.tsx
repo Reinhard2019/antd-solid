2eb6bae203c5a632c7a072a7fd1b121082a95304
丁磊 <XNBigDevil@gmail.com>
2023-12-27 18:28:40 +0800

    "version": "0.0.7"

--- src/DatePicker.tsx
+++ src/DatePicker.tsx
@@ -1,9 +1,27 @@
 import { DatePicker as DatePickerAntd } from 'antd'
+import { type DatePickerProps, type RangePickerProps } from 'antd/es/date-picker'
 import { reactToSolidComponent, replaceClassName } from './utils/component'
 
-const RangePicker = replaceClassName(reactToSolidComponent(DatePickerAntd.RangePicker))
+const RangePicker = replaceClassName(
+  reactToSolidComponent<
+  RangePickerProps & {
+    dropdownClassName?: string | undefined
+    popupClassName?: string | undefined
+    rootClassName?: string | undefined
+  }
+  >(DatePickerAntd.RangePicker),
+)
 
-const _DatePicker = replaceClassName(reactToSolidComponent(DatePickerAntd))
+const _DatePicker = replaceClassName(
+  reactToSolidComponent<
+  DatePickerProps & {
+    status?: '' | 'error' | 'warning' | undefined
+    hashId?: string | undefined
+    popupClassName?: string | undefined
+    rootClassName?: string | undefined
+  }
+  >(DatePickerAntd),
+)
 const DatePicker = _DatePicker as typeof _DatePicker & {
   RangePicker: typeof RangePicker
 }
