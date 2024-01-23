9fcdc90ed9e966ab43f151e77be81a290fcb12ca
丁磊 <XNBigDevil@gmail.com>
2024-01-23 11:11:43 +0800

    🦄 refactor(Select):

---
+++ src/Select/SelectInput.tsx
@@ -0,0 +1,148 @@
+import { type JSXElement, For, createSignal, Show, createMemo } from 'solid-js'
+import Tooltip from '../Tooltip'
+import createControllableValue from '../hooks/createControllableValue'
+import cs from 'classnames'
+import { useClickAway } from '../hooks'
+import { compact } from 'lodash-es'
+import Compact from '../Compact'
+
+export interface SelectInputProps<T> {
+  multiple?: boolean
+  defaultValue?: T[]
+  value?: T[]
+  onChange?: (value: T[]) => void
+  optionLabelRender: (v: T) => JSXElement
+  placeholder?: string
+  allowClear?: boolean
+  disabled?: boolean
+  class?: string
+  content: (close: () => void) => JSXElement
+}
+
+function SelectInput<T>(props: SelectInputProps<T>) {
+  let select: HTMLDivElement
+  let tooltipContent: HTMLDivElement
+
+  const [value, setValue] = createControllableValue<T[] | undefined>(props, {
+    defaultValue: [],
+  })
+  const valueArr = createMemo(() => value() ?? [])
+
+  const [open, setOpen] = createSignal(false)
+  useClickAway(
+    () => setOpen(false),
+    () => compact([select, tooltipContent]),
+  )
+
+  const [width, setWidth] = createSignal(0)
+  const [hover, setHover] = createSignal(false)
+  const showClearBtn = createMemo(() => props.allowClear && hover() && valueArr().length > 0)
+
+  return (
+    <div
+      class={cs(
+        'rounded-6px',
+        [
+          Compact.compactItemClass,
+          Compact.compactItemRounded0Class,
+          'p[.ant-compact]:first:rounded-l-6px',
+          'p[.ant-compact]:last:rounded-r-6px',
+          Compact.compactItemZIndexClass,
+        ],
+        props.class,
+        props.disabled && 'cursor-not-allowed',
+      )}
+      style={{
+        '--ant-select-multiple-item-bg': 'rgba(0, 0, 0, 0.06)',
+        '--ant-select-multiple-item-height': '24px',
+      }}
+    >
+      <Tooltip
+        mode="light"
+        open={open()}
+        onOpenChange={setOpen}
+        trigger={[]}
+        placement="bottomLeft"
+        arrow={false}
+        contentStyle={{
+          padding: 0,
+        }}
+        content={close => (
+          <div
+            ref={tooltipContent}
+            class="bg-white w-200px max-h-400px overflow-auto"
+            style={{ width: `${width()}px` }}
+          >
+            {props.content(close)}
+          </div>
+        )}
+      >
+        <div
+          ref={select!}
+          class={cs(
+            'relative h-32px leading-32px [border:1px_solid_var(--ant-color-border)] pr-25px hover:border-[var(--ant-color-primary)] focus:[border-color:var(--ant-color-primary)] focus:[box-shadow:0_0_0_2px_var(--ant-control-outline)] rounded-inherit',
+            valueArr().length && props.multiple ? 'pl-4px' : 'pl-11px',
+            props.disabled &&
+              '[pointer-events:none] bg-[var(--ant-color-bg-container-disabled)] color-[var(--ant-color-text-disabled)]',
+          )}
+          tabIndex="0"
+          onClick={e => {
+            setOpen(true)
+            setWidth(e.currentTarget.offsetWidth)
+            e.currentTarget.focus()
+          }}
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+        >
+          <div class="h-full ellipsis">
+            <Show
+              when={valueArr().length}
+              fallback={
+                <input
+                  class="h-full w-full float-left [outline:none] bg-inherit placeholder-text-[rgba(0,0,0,.25)]"
+                  readOnly
+                  placeholder={props.placeholder}
+                />
+              }
+            >
+              <Show when={props.multiple} fallback={props.optionLabelRender(valueArr()[0])}>
+                <div class="flex gap-4px h-full items-center">
+                  <For each={valueArr()}>
+                    {item => (
+                      <span class="bg-[var(--ant-select-multiple-item-bg)] leading-[var(--ant-select-multiple-item-height)] h-[var(--ant-select-multiple-item-height)] pl-8px pr-4px rounded-[var(--ant-border-radius-sm)]">
+                        {props.optionLabelRender(item)}
+                        <span
+                          class="i-ant-design:close-outlined text-[var(--ant-color-icon)] hover:text-[var(--ant-color-icon-hover)] text-12px cursor-pointer"
+                          onClick={() => setValue(valueArr().filter(v => v !== item))}
+                        />
+                      </span>
+                    )}
+                  </For>
+                </div>
+              </Show>
+            </Show>
+          </div>
+
+          <div class="absolute top-0 bottom-0 right-11px">
+            <Show
+              when={showClearBtn()}
+              fallback={
+                <span class="i-ant-design:down-outlined text-[var(--ant-color-text-quaternary)]" />
+              }
+            >
+              <span
+                class="i-ant-design:close-circle-filled cursor-pointer text-[var(--ant-color-text-quaternary)] hover:text-[var(--ant-color-text-tertiary)]"
+                onClick={e => {
+                  e.stopPropagation()
+                  setValue([])
+                }}
+              />
+            </Show>
+          </div>
+        </div>
+      </Tooltip>
+    </div>
+  )
+}
+
+export default SelectInput
--- src/Select/index.tsx
+++ src/Select/index.tsx
@@ -3,40 +3,40 @@ import {
   type Component,
   For,
   createSelector,
-  createSignal,
-  Show,
   createMemo,
+  splitProps,
 } from 'solid-js'
-import Tooltip from '../Tooltip'
 import { type Key } from '../types'
 import createControllableValue from '../hooks/createControllableValue'
 import cs from 'classnames'
-import { useClickAway } from '../hooks'
-import { compact, keyBy } from 'lodash-es'
+import { keyBy } from 'lodash-es'
 import { toArray } from '../utils/array'
-import Compact from '../Compact'
+import SelectInput, { type SelectInputProps } from './SelectInput'
 
 interface SelectOption {
   label: JSXElement
   value: Key
 }
 
-interface SelectProps {
-  multiple?: boolean
+interface SelectProps
+  extends Omit<
+  SelectInputProps<Key>,
+  'defaultValue' | 'value' | 'onChange' | 'optionLabelRender' | 'content'
+  > {
   defaultValue?: Key | Key[]
   value?: Key | Key[]
   onChange?: (value: Key | Key[]) => void
   options: SelectOption[]
-  placeholder?: string
-  allowClear?: boolean
-  disabled?: boolean
-  class?: string
 }
 
 const Select: Component<SelectProps> = props => {
-  let select: HTMLDivElement
-  let tooltipContent: HTMLDivElement
-
+  const [selectInputProps] = splitProps(props, [
+    'multiple',
+    'allowClear',
+    'class',
+    'disabled',
+    'placeholder',
+  ])
   const [value, setValue] = createControllableValue<Key | Key[] | undefined>(props)
   const valueArr = createMemo(() => toArray(value(), false) as Key[])
   const selectedValue = createSelector<Map<Key, true>, Key>(
@@ -44,146 +44,45 @@ const Select: Component<SelectProps> = props => {
     (a, b) => b.has(a),
   )
   const optionDict = createMemo(() => keyBy(props.options, v => v.value))
-  const getOptionLabel = (v: Key) => optionDict()[v]?.label ?? v
-
-  const [open, setOpen] = createSignal(false)
-  useClickAway(
-    () => setOpen(false),
-    () => compact([select, tooltipContent]),
-  )
-
-  const [width, setWidth] = createSignal(0)
-  const [hover, setHover] = createSignal(false)
-  const showClearBtn = createMemo(() => props.allowClear && hover() && valueArr().length > 0)
 
   return (
-    <div
-      class={cs(
-        'rounded-6px',
-        [
-          Compact.compactItemClass,
-          Compact.compactItemRounded0Class,
-          'p[.ant-compact]:first:rounded-l-6px',
-          'p[.ant-compact]:last:rounded-r-6px',
-          Compact.compactItemZIndexClass,
-        ],
-        props.class,
-        props.disabled && 'cursor-not-allowed',
-      )}
-      style={{
-        '--ant-select-multiple-item-bg': 'rgba(0, 0, 0, 0.06)',
-        '--ant-select-multiple-item-height': '24px',
+    <SelectInput<Key>
+      {...selectInputProps}
+      optionLabelRender={v => optionDict()[v]?.label ?? v}
+      value={valueArr()}
+      onChange={v => {
+        setValue(props.multiple ? v : v[0])
       }}
-    >
-      <Tooltip
-        mode="light"
-        open={open()}
-        onOpenChange={setOpen}
-        trigger={[]}
-        placement="bottomLeft"
-        arrow={false}
-        contentStyle={{
-          padding: 0,
-        }}
-        content={close => (
-          <div
-            ref={tooltipContent}
-            class="bg-white w-200px p-2px"
-            style={{ width: `${width()}px` }}
-          >
-            <For each={props.options}>
-              {item => (
-                <div
-                  class={cs(
-                    'ellipsis box-content px-12px py-5px h-22px leading-22px hover:bg-[var(--ant-hover-bg-color)] cursor-pointer rounded-[var(--ant-border-radius-sm)]',
-                    selectedValue(item.value) ? '!bg-[var(--ant-select-option-selected-bg)]' : '',
-                  )}
-                  onClick={() => {
-                    if (!props.multiple) {
-                      setValue(item.value)
-                      close()
-                      return
-                    }
+      content={close => (
+        <div class="p-2px">
+          <For each={props.options}>
+            {item => (
+              <div
+                class={cs(
+                  'ellipsis box-content px-12px py-5px h-22px leading-22px hover:bg-[var(--ant-hover-bg-color)] cursor-pointer rounded-[var(--ant-border-radius-sm)]',
+                  selectedValue(item.value) ? '!bg-[var(--ant-select-option-selected-bg)]' : '',
+                )}
+                onClick={() => {
+                  if (!props.multiple) {
+                    setValue(item.value)
+                    close()
+                    return
+                  }
 
-                    if (valueArr().includes(item.value)) {
-                      setValue(valueArr().filter(v => v !== item.value))
-                    } else {
-                      setValue([...valueArr(), item.value])
-                    }
-                  }}
-                >
-                  {item.label}
-                </div>
-              )}
-            </For>
-          </div>
-        )}
-      >
-        <div
-          ref={select!}
-          class={cs(
-            'relative h-32px leading-32px [border:1px_solid_var(--ant-color-border)] pr-25px hover:border-[var(--ant-color-primary)] focus:[border-color:var(--ant-color-primary)] focus:[box-shadow:0_0_0_2px_var(--ant-control-outline)] rounded-inherit',
-            valueArr().length && props.multiple ? 'pl-4px' : 'pl-11px',
-            props.disabled &&
-              '[pointer-events:none] bg-[var(--ant-color-bg-container-disabled)] color-[var(--ant-color-text-disabled)]',
-          )}
-          tabIndex="0"
-          onClick={e => {
-            setOpen(true)
-            setWidth(e.currentTarget.offsetWidth)
-            e.currentTarget.focus()
-          }}
-          onMouseEnter={() => setHover(true)}
-          onMouseLeave={() => setHover(false)}
-        >
-          <div class="h-full ellipsis">
-            <Show
-              when={valueArr().length}
-              fallback={
-                <input
-                  class="h-full w-full float-left [outline:none] bg-inherit placeholder-text-[rgba(0,0,0,.25)]"
-                  readOnly
-                  placeholder={props.placeholder}
-                />
-              }
-            >
-              <Show when={props.multiple} fallback={getOptionLabel(valueArr()[0])}>
-                <div class="flex gap-4px h-full items-center">
-                  <For each={valueArr()}>
-                    {item => (
-                      <span class="bg-[var(--ant-select-multiple-item-bg)] leading-[var(--ant-select-multiple-item-height)] h-[var(--ant-select-multiple-item-height)] pl-8px pr-4px rounded-[var(--ant-border-radius-sm)]">
-                        {getOptionLabel(item)}
-                        <span
-                          class="i-ant-design:close-outlined text-[var(--ant-color-icon)] hover:text-[var(--ant-color-icon-hover)] text-12px cursor-pointer"
-                          onClick={() => setValue(valueArr().filter(v => v !== item))}
-                        />
-                      </span>
-                    )}
-                  </For>
-                </div>
-              </Show>
-            </Show>
-          </div>
-
-          <div class="absolute top-0 bottom-0 right-11px">
-            <Show
-              when={showClearBtn()}
-              fallback={
-                <span class="i-ant-design:down-outlined text-[var(--ant-color-text-quaternary)]" />
-              }
-            >
-              <span
-                class="i-ant-design:close-circle-filled cursor-pointer text-[var(--ant-color-text-quaternary)] hover:text-[var(--ant-color-text-tertiary)]"
-                onClick={e => {
-                  e.stopPropagation()
-                  setValue(props.multiple ? [] : undefined)
+                  if (valueArr().includes(item.value)) {
+                    setValue(valueArr().filter(v => v !== item.value))
+                  } else {
+                    setValue([...valueArr(), item.value])
+                  }
                 }}
-              />
-            </Show>
-          </div>
+              >
+                {item.label}
+              </div>
+            )}
+          </For>
         </div>
-      </Tooltip>
-    </div>
+      )}
+    />
   )
 }
 
