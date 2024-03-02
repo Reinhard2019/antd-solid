345489bcc7a6c1f75fae1c285a720ed912f0abd7
丁磊 <XNBigDevil@gmail.com>
2024-03-02 11:18:00 +0800

    ✨ feat: add export TextAreaProps

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { default as Button } from './Button'
 export { default as Input } from './Input'
-export type { InputProps } from './Input'
+export type { InputProps, TextAreaProps } from './Input'
 export { default as InputNumber } from './InputNumber'
 export type { InputNumberProps } from './InputNumber'
 export { default as Timeline } from './Timeline'
