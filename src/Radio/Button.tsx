f03dfbca169fb788029f66eefccf2c876cf22967
丁磊 <XNBigDevil@gmail.com>
2024-03-09 10:12:45 +0800

    🐞 fix(Radio): css style

--- src/Radio/Button.tsx
+++ src/Radio/Button.tsx
@@ -5,6 +5,8 @@ import { type RadioProps } from '.'
 import { wave } from '../utils/animation'
 
 const Button: Component<RadioProps> = props => {
+  let input: HTMLInputElement | undefined
+
   const [checked, setChecked] = createControllableValue(props, {
     defaultValue: false,
     defaultValuePropName: 'defaultChecked',
@@ -15,7 +17,7 @@ const Button: Component<RadioProps> = props => {
   return (
     <label
       class={cs(
-        'relative h-32px [border:1px_solid_rgb(217,217,217)] first:rounded-l-6px last:rounded-r-6px cursor-pointer flex-grow not[:first-child]:-ml-1px',
+        'relative px-15px h-32px [border:1px_solid_rgb(217,217,217)] first:rounded-l-6px last:rounded-r-6px cursor-pointer flex-grow not[:first-child]:-ml-1px flex justify-center items-center',
         checked() &&
           cs(
             props.disabled
@@ -26,8 +28,13 @@ const Button: Component<RadioProps> = props => {
           ? 'cursor-not-allowed [border:1px_solid_var(--ant-color-border)] bg-[var(--ant-color-bg-container-disabled)] text-[var(--ant-color-text-disabled)]'
           : 'hover:text-[var(--ant-color-primary)]',
       )}
+      onClick={e => {
+        if (e.target === input) return
+        wave(e.currentTarget, 'var(--ant-color-primary)')
+      }}
     >
       <input
+        ref={input}
         class="hidden"
         checked={checked()}
         type="radio"
@@ -37,14 +44,8 @@ const Button: Component<RadioProps> = props => {
           props.onChange?.(e)
         }}
       />
-      <span
-        class="inline-flex items-center justify-center px-15px absolute inset-0 rounded-inherit"
-        onClick={e => {
-          wave(e.currentTarget, 'var(--ant-color-primary)')
-        }}
-      >
-        {props.children}
-      </span>
+
+      <span>{props.children}</span>
     </label>
   )
 }
