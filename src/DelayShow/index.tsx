585dfa9cc8c7bcfdb9473aa040ed62fcfafd8c35
丁磊 <XNBigDevil@gmail.com>
2024-05-04 21:34:15 +0800

    ✨ feat(DelayShow): add props.time

--- src/DelayShow/index.tsx
+++ src/DelayShow/index.tsx
@@ -2,22 +2,41 @@
  * 延迟创建组件
  * 只有当 props.when 为 true 时才创建组件
  */
-import { type Component, type JSXElement, Show, untrack, createEffect } from 'solid-js'
+import {
+  type Component,
+  type JSXElement,
+  Show,
+  untrack,
+  createSignal,
+  createRenderEffect,
+} from 'solid-js'
 
 export interface DelayShowProps {
   when?: boolean
+  /**
+   * 延迟显示的时间
+   * 单位 ms
+   */
+  time?: number
   children: JSXElement
 }
 
 const DelayShow: Component<DelayShowProps> = props => {
   // 是否显示过一次
-  let showed = untrack(() => props.when ?? false)
-  createEffect(() => {
+  const [showed, setShowed] = createSignal(untrack(() => props.when ?? false))
+  createRenderEffect(() => {
     if (props.when) {
-      showed = true
+      setShowed(true)
     }
   })
-  return <Show when={showed || props.when}>{props.children}</Show>
+  createRenderEffect(() => {
+    if (typeof props.time === 'number') {
+      setTimeout(() => {
+        setShowed(true)
+      }, props.time)
+    }
+  })
+  return <Show when={showed()}>{props.children}</Show>
 }
 
 export default DelayShow
