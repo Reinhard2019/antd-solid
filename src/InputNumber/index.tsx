0ad37b34e9abafbf33f37b48f62ab2366df9a04c
丁磊 <XNBigDevil@gmail.com>
2024-07-23 09:05:21 +0800

    🐞 fix(InputNumber): 外部传入的 onFocus 没有执行

--- src/InputNumber/index.tsx
+++ src/InputNumber/index.tsx
@@ -153,7 +153,8 @@ const InputNumber: Component<InputNumberProps> = _props => {
         setValue(newValue)
         updateValidValue(newValue)
       }}
-      onFocus={() => {
+      onFocus={e => {
+        dispatchEventHandlerUnion(props.onFocus, e)
         setInFocus(true)
       }}
       onBlur={e => {
