17fbbdad6e48d131f201cd098fdae6918b79d198
丁磊 <XNBigDevil@gmail.com>
2024-07-28 13:28:53 +0800

    🐞 fix(useSize): ResizeObserver callback 首次执行不及时的问题

--- src/hooks/useSize.ts
+++ src/hooks/useSize.ts
@@ -1,21 +1,31 @@
+import { isEqual } from 'lodash-es'
 import { type Accessor, createSignal, onCleanup, createEffect } from 'solid-js'
 
 export default function useSize(target: Accessor<Element | undefined>) {
-  const [size, setSize] = createSignal<{
+  const [size, setSize] = createSignal<
+  | {
     width: number
     height: number
-  }>()
+  }
+  | undefined
+  >(undefined, {
+    equals: isEqual,
+  })
 
   createEffect(() => {
     const _target = target()
     if (!_target) return
 
-    const ro = new ResizeObserver(() => {
+    const callback = () => {
       setSize({
         width: _target.clientWidth,
         height: _target.clientHeight,
       })
-    })
+    }
+    // ResizeObserver observe 后虽然也会立刻执行 callback，但它执行不及时
+    callback()
+
+    const ro = new ResizeObserver(callback)
     ro.observe(_target)
     onCleanup(() => {
       ro.disconnect()
