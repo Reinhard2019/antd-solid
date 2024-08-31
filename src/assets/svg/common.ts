2e9e684b49c315af08258398fc8cb0339bb76b18
丁磊 <XNBigDevil@gmail.com>
2024-08-31 09:52:51 +0800

    🐞 fix(Svg): css style

--- src/assets/svg/common.ts
+++ src/assets/svg/common.ts
@@ -3,6 +3,7 @@ import { type JSX } from 'solid-js'
 export const commonStyle: JSX.CSSProperties = {
   width: '1em',
   height: '1em',
+  fill: 'currentColor',
   display: 'inline-block',
   'vertical-align': '-0.125em',
 }
