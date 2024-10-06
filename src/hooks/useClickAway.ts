135763ce50f41c31be7a9fb65475948fe04af85a
丁磊 <XNBigDevil@gmail.com>
2024-10-06 08:12:16 +0800

    ✨ feat(useClickAway): target 可以返回 undefined

--- src/hooks/useClickAway.ts
+++ src/hooks/useClickAway.ts
@@ -1,12 +1,13 @@
 import { type Accessor, onCleanup } from 'solid-js'
+import { compact } from 'lodash-es'
 import { toArray } from '../utils/array'
 
 export default function useClickAway<T extends Event = Event>(
   onClickAway: (event: T) => void,
-  target?: Accessor<Element | Element[]>,
+  target?: Accessor<Element | Element[] | undefined>,
 ) {
   const onClick = (event: Event) => {
-    const targets = target ? toArray(target()) : []
+    const targets = compact(target ? toArray(target()) : [])
     if (targets.every(item => !item.contains(event.target as Node))) {
       onClickAway(event as T)
     }
