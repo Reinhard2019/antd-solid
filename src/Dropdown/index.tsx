d0652a234638f9142b444e223412f9eb3f14ddaf
丁磊 <XNBigDevil@gmail.com>
2024-11-10 20:15:00 +0800

    🐞 fix(Dropdown): Dropdown 下拉容器 width 不应小于触发节点

--- src/Dropdown/index.tsx
+++ src/Dropdown/index.tsx
@@ -16,6 +16,7 @@ export interface DropdownProps extends Omit<PopoverProps, 'placement'> {
   placement?: 'top' | 'bottom' | 'topLeft' | 'topRight' | 'bottomLeft' | 'bottomRight'
   /**
    * 下拉菜单和选择器同宽
+   * 默认 true
    */
   popupMatchWidth?: boolean
 }
@@ -26,6 +27,7 @@ const Dropdown: Component<DropdownProps> = _props => {
   const props = mergeProps(
     {
       placement: 'bottomLeft' as const,
+      popupMatchWidth: true,
     },
     _props,
   )
@@ -40,7 +42,8 @@ const Dropdown: Component<DropdownProps> = _props => {
             {...props.menu}
             style={{
               ...props.menu.style,
-              'min-width': props.popupMatchWidth ? `${size()?.width ?? 0}px` : undefined,
+              'min-width': `${size()?.width ?? 0}px`,
+              width: props.popupMatchWidth ? `${size()?.width ?? 0}px` : undefined,
               padding: '4px',
             }}
             onClick={info => {
