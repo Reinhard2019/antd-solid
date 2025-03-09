1411d6dcc1dd1ee90e0aa2642a240640831ebe55
丁磊 <XNBigDevil@gmail.com>
2025-03-09 14:17:38 +0800

    🌈 style(Result): update css style

--- src/Result/index.tsx
+++ src/Result/index.tsx
@@ -1,4 +1,4 @@
-import { type JSXElement, type Component, type ParentProps } from 'solid-js'
+import { type JSXElement, type Component, type ParentProps, mergeProps } from 'solid-js'
 import cs from 'classnames'
 import Element from '../Element'
 
@@ -12,22 +12,28 @@ export interface ResultProps extends ParentProps {
 }
 
 const statusIconMap = {
-  success: 'text-#52c41a i-ant-design:check-circle-filled',
-  info: 'text-[var(--ant-color-primary)] i-ant-design:exclamation-circle-filled',
-  warning: 'text-#faad14 i-ant-design:warning-filled',
-  error: 'text-#ff4d4f i-ant-design:close-circle-filled',
+  success: '!text-[--ant-color-success] i-ant-design:check-circle-filled',
+  info: '!text-[--ant-color-primary] i-ant-design:exclamation-circle-filled',
+  warning: '!text-[--ant-color-warning] i-ant-design:warning-filled',
+  error: '!text-[--ant-color-error] i-ant-design:close-circle-filled',
 }
 
-const Result: Component<ResultProps> = props => {
+const Result: Component<ResultProps> = _props => {
+  const props = mergeProps(
+    {
+      status: 'info' as const,
+    },
+    _props,
+  )
   return (
-    <Element class="text-center px-32px py-48px">
+    <Element class="text-center px-32px py-48px [font-size:var(--ant-font-size)] text-[--ant-color-text] leading-[--ant-line-height]">
       <div class="mb-24px">
         <span class={cs(statusIconMap[props.status!], 'text-72px')} />
       </div>
 
-      <div class="my-8px text-[rgba(0,0,0,.88)] text-24px">{props.title}</div>
+      <div class="my-8px text-[--ant-color-text-heading] text-24px">{props.title}</div>
 
-      <div class="text-[rgba(0,0,0,.45)] text-14px">{props.subTitle}</div>
+      <div class="text-[--ant-color-text-description] text-14px">{props.subTitle}</div>
 
       <div class="mt-24px">{props.extra}</div>
 
