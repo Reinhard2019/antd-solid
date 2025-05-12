b2ce967b354171b109a37a736781c9c652317fc3
丁磊 <XNBigDevil@gmail.com>
2025-05-12 10:19:57 +0800

    🎈 perf(Fragment): 扩充 FragmentProps

--- src/Fragment/index.tsx
+++ src/Fragment/index.tsx
@@ -1,6 +1,13 @@
 import { type Component, type ParentProps } from 'solid-js'
 
-const Fragment: Component<ParentProps> = props => {
+// 增加 Record<string, any> 是为了防止下面的情况不报错
+// <Dynamic
+//  component={is ? Other : Fragment}
+//  otherProp={1}
+// />
+type FragmentProps = ParentProps & Record<string, any>
+
+const Fragment: Component<FragmentProps> = props => {
   return <>{props.children}</>
 }
 
