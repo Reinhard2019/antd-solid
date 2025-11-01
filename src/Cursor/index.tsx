cdd805d8fc140ff670583fc109fc7805369b12e6
丁磊 <XNBigDevil@gmail.com>
2025-11-01 14:07:02 +0800

    feat: add Cursor.createCursor

--- src/Cursor/index.tsx
+++ src/Cursor/index.tsx
@@ -7,14 +7,38 @@ import {
   children,
   createEffect,
   onCleanup,
+  type Ref,
 } from 'solid-js'
-import { Portal } from 'solid-js/web'
+import { Portal, render } from 'solid-js/web'
+import { setRef } from '../utils/solid'
+
+export interface CursorIntance {
+  onMouseEnter: (e: MouseEvent, ele: HTMLElement | SVGElement) => void
+  onMouseMove: (e: MouseEvent) => void
+  onMouseLeave: (e: MouseEvent, ele: HTMLElement | SVGElement) => void
+}
 
 export interface CursorProps extends ParentProps {
+  ref?: Ref<CursorIntance>
   cursor: JSX.Element
 }
 
-const Cursor: Component<CursorProps> = props => {
+function createCursor(props: CursorProps) {
+  const container = document.createElement('div')
+  document.body.appendChild(container)
+  const _dispose = render(() => <Cursor {...props} />, container)
+  const dispose = () => {
+    _dispose()
+    document.body.removeChild(container)
+  }
+  return {
+    dispose,
+  }
+}
+
+const Cursor: Component<CursorProps> & {
+  createCursor: typeof createCursor
+} = props => {
   const resolvedChildren = children(() => props.children)
 
   const [hover, setHover] = createSignal(false)
@@ -25,6 +49,37 @@ const Cursor: Component<CursorProps> = props => {
 
   let originalCursor: string | undefined
 
+  const onMouseEnter = (_: MouseEvent, _children: HTMLElement | SVGElement) => {
+    setHover(true)
+
+    originalCursor = _children.style.cursor
+    _children.style.cursor = 'none'
+  }
+
+  const onMouseMove = (e: MouseEvent) => {
+    setCursorPosition({
+      x: e.clientX,
+      y: e.clientY,
+    })
+  }
+
+  const onMouseLeave = (_: MouseEvent, _children: HTMLElement | SVGElement) => {
+    setHover(false)
+
+    if (originalCursor) {
+      _children.style.cursor = originalCursor
+    } else {
+      _children.style.removeProperty('cursor')
+    }
+    originalCursor = undefined
+  }
+
+  setRef(props, {
+    onMouseEnter,
+    onMouseMove,
+    onMouseLeave,
+  })
+
   createEffect(() => {
     const _children = resolvedChildren()
     if (!(_children instanceof HTMLElement || _children instanceof SVGElement)) return
@@ -33,38 +88,24 @@ const Cursor: Component<CursorProps> = props => {
 
     _children.addEventListener(
       'mouseenter',
-      () => {
-        setHover(true)
-
-        originalCursor = _children.style.cursor
-        _children.style.cursor = 'none'
-      },
-      {
-        capture: true,
-        signal: abortController.signal,
-      },
-    )
-
-    _children.addEventListener(
-      'mousemove',
       (e: MouseEvent) => {
-        setCursorPosition({
-          x: e.clientX,
-          y: e.clientY,
-        })
+        onMouseEnter(e, _children)
       },
       {
         capture: true,
         signal: abortController.signal,
       },
     )
+
+    _children.addEventListener('mousemove', onMouseMove, {
+      capture: true,
+      signal: abortController.signal,
+    })
 
     _children.addEventListener(
       'mouseleave',
-      () => {
-        setHover(false)
-
-        if (originalCursor) _children.style.cursor = originalCursor
+      (e: MouseEvent) => {
+        onMouseLeave(e, _children)
       },
       {
         capture: true,
@@ -97,5 +138,7 @@ const Cursor: Component<CursorProps> = props => {
     </>
   )
 }
+
+Cursor.createCursor = createCursor
 
 export default Cursor
